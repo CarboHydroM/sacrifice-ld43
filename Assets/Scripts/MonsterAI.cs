550842cbd6ab2a3ff046d35d515c2e1ce11d84a8
lhamot <loic.hamot@yahoo.fr>
2018-12-01 17:00:26 +0100

    Monster can now move

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -11,6 +11,13 @@ public class MonsterAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //Debug.Log("Hello", gameObject);
+
+        MonsterController controller = gameObject.GetComponent<MonsterController>();
+        //Vector2 dir = new Vector2(Mathf.Cos(Time.fixedTime), Mathf.Sin(Time.fixedTime));
+        Vector2 dir = new Vector2(Mathf.Cos(Time.fixedTime), -1f);
+        dir.Normalize();
+        controller.MoveDir(dir);
+    }
 }
