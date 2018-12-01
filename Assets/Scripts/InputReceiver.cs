0a1ecd3576b660e399d73dad990592e42e9c597b
Christophe Blondel <christophe@carbohydromusic.com>
2018-12-01 19:08:01 +0100

    Input manager settings / Player + PlayerBullet objects

--- Assets/Scripts/InputReceiver.cs
+++ Assets/Scripts/InputReceiver.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class InputReceiver : MonoBehaviour {
 
+	private Vector2 m_firingDirection;
+	private float m_firingClock;
+	public float m_firingThroughput = 0.2f;
+	public GameObject m_bulletPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+		m_firingDirection = new Vector2(0f, 1f);
+		m_firingClock = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float dh = Input.GetAxis("Horizontal");
-		float dv = Input.GetAxis("Vertical");
+		float ix = Input.GetAxis("Horizontal 1");
+		float iy = Input.GetAxis("Vertical 1");
+		float sx = Input.GetAxis("Horizontal 2");
+		float sy = Input.GetAxis("Vertical 2");
+		bool fire = Input.GetKeyDown(KeyCode.A);
 
-		Vector2 impulse = new Vector2(dh, dv);
-		Vector2 move = impulse * Time.deltaTime;
+		Vector2 impulse = new Vector2(ix, iy);
+		m_firingDirection = new Vector2(sx, sy);
+		m_firingDirection.Normalize();
+
+		float dt = Time.deltaTime;
+		Vector2 move = impulse * dt;
+		m_firingClock += dt;
 
 		Transform t = gameObject.GetComponent(typeof(Transform)) as Transform;
 		t.position += new Vector3(move.x, move.y);
+
+		if (fire) {
+			if (m_firingClock >= m_firingThroughput) {
+				GameObject bObject = Instantiate(m_bulletPrefab, t.position, Quaternion.identity);
+				Projectile bullet = bObject.GetComponent(typeof(Projectile)) as Projectile;
+				bullet.Shoot(m_firingDirection);
+				m_firingClock = 0f;
+			}
+		} else
+			m_firingClock = 0f;
 	}
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	private Vector2 m_direction;
+	public float m_speed;
+
+	public void Shoot(Vector2 direction) {
+		m_direction = direction;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 move = m_direction * m_speed * Time.deltaTime;
+
+		Transform t = gameObject.GetComponent(typeof(Transform)) as Transform;
+		t.position += new Vector3(move.x, move.y);
+
+		// V0 checking here that we are out of range and ready to despawn
+		Camera c = Camera.main;
+		if (t.position.y >= c.pixelHeight)
+			Destroy(gameObject);
+	}
+}
