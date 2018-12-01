d21ce9a1d6a2a7259bdf12ce06643d539e336e1b
Christophe Blondel <christophe@carbohydromusic.com>
2018-12-01 16:51:47 +0100

    Basic player that can be moved with input axes

--- Assets/Scripts/InputReceiver.cs
+++ Assets/Scripts/InputReceiver.cs
@@ -11,6 +11,13 @@ public class InputReceiver : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float dh = Input.GetAxis("Horizontal");
+		float dv = Input.GetAxis("Vertical");
+
+		Vector2 impulse = new Vector2(dh, dv);
+		Vector2 move = impulse * Time.deltaTime;
+
+		Transform t = gameObject.GetComponent(typeof(Transform)) as Transform;
+		t.position += new Vector3(move.x, move.y);
 	}
 }
