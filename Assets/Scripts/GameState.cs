f450d38e5a12697b2e7446cfc618429bafe9df20
lhamot <loic.hamot@yahoo.fr>
2018-12-02 13:34:21 +0100

    Add the MainMenu

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour {
+
+    public GameObject mainMenuCanvas;
+    public GameObject ingameCanvas;
+    public GameObject partyPrefab;
+
+    enum State
+    {
+        MainMenu,
+        InGame,
+        InGameMenu,
+    }
+
+    State m_state = State.MainMenu;
+
+    // Use this for initialization
+    void Start () {
+        mainMenuCanvas.SetActive(true);
+        ingameCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OnStartParty()
+    {
+        mainMenuCanvas.SetActive(false);
+        ingameCanvas.SetActive(true);
+        Instantiate(partyPrefab);
+
+        SceneManager.LoadScene("Scenes/Level1", LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Party.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Party : MonoBehaviour {
+    public int[] score = { 0, 0, 0, 0 };
+
+    public void Reset()
+    {
+        score = new int[] { 0, 0, 0, 0 };
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
