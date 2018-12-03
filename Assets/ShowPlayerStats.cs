1dd03903e84739a73bf1b683972f30dc8cbb57c8
Christophe Blondel <christophe@carbohydromusic.com>
2018-12-03 18:39:50 +0100

    Bugfix (do not reuse wastedAmmo without overwriting its previous value)

--- Assets/ShowPlayerStats.cs
+++ Assets/ShowPlayerStats.cs
@@ -27,24 +27,32 @@ public class ShowPlayerStats : MonoBehaviour {
         float consumed = partyStat.ammoConsumption[0];
         if (consumed > 0f)
             wasteRatio = partyStat.wastedAmmo[0] * 100f / consumed;
+        else
+            wasteRatio = 0f;
         string statsText = "Kills: " + partyStat.ennemyKills[0].ToString() +
                             " / Wasted ammo: " + wasteRatio + "%";
         redStats.GetComponent<Text>().text = statsText;
         consumed = partyStat.ammoConsumption[1];
         if (consumed > 0f)
             wasteRatio = partyStat.wastedAmmo[1] * 100f / consumed;
+        else
+            wasteRatio = 0f;
         statsText = "Kills: " + partyStat.ennemyKills[1].ToString() +
                             " / Wasted ammo: " + wasteRatio + "%";
         blueStats.GetComponent<Text>().text = statsText;
         consumed = partyStat.ammoConsumption[2];
         if (consumed > 0f)
             wasteRatio = partyStat.wastedAmmo[2] * 100f / consumed;
+        else
+            wasteRatio = 0f;
         statsText = "Kills: " + partyStat.ennemyKills[2].ToString() +
                             " / Wasted ammo: " + wasteRatio + "%";
         yellowStats.GetComponent<Text>().text = statsText;
         consumed = partyStat.ammoConsumption[3];
         if (consumed > 0f)
             wasteRatio = partyStat.wastedAmmo[3] * 100f / consumed;
+        else
+            wasteRatio = 0f;
         statsText = "Kills: " + partyStat.ennemyKills[3].ToString() +
                             " / Wasted ammo: " + wasteRatio + "%";
         greenStats.GetComponent<Text>().text = statsText;
