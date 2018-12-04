be707aae7b9730821788e063b31f253841a821ee
lhamot <loic.hamot@yahoo.fr>
2018-12-04 03:28:00 +0100

    Fix line endings in BackgroundScrolling

--- Assets/Scripts/BackgroundScrolling.cs
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundScrolling : MonoBehaviour {
+public class BackgroundScrolling : MonoBehaviour
+{
 
     public Sprite[] lvlBackgrounds;
     public float startY;    // <---- hypergruik
     private float startAltitude;
     private SpriteRenderer spriteRenderer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         GameObject query = GameObject.FindGameObjectWithTag("Party");
         PartyStat partyStat = query.GetComponent<PartyStat>();
         Transform t = gameObject.GetComponent<Transform>();
         float alt = partyStat.altitude - startAltitude;
         Vector3 pos = new Vector3(0f, startY - (alt * 0.25f), 30f);
         t.position = pos;
-	}
+    }
 
     public void SetLevelBackground(int lvlIndex)
     {
