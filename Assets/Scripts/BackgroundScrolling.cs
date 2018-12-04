9fc5354feba6ab4fa1bed4099fd2b0bb80dba1bf
Christophe Blondel <christophe@carbohydromusic.com>
2018-12-04 03:19:53 +0100

    Merge master

--- Assets/Scripts/BackgroundScrolling.cs
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -6,6 +6,7 @@ public class BackgroundScrolling : MonoBehaviour {
 
     public Sprite[] lvlBackgrounds;
     public float startY;    // <---- hypergruik
+    private float startAltitude;
     private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
@@ -18,12 +19,16 @@ public class BackgroundScrolling : MonoBehaviour {
         GameObject query = GameObject.FindGameObjectWithTag("Party");
         PartyStat partyStat = query.GetComponent<PartyStat>();
         Transform t = gameObject.GetComponent<Transform>();
-        Vector3 pos = new Vector3(0f, startY - (partyStat.altitude * 0.25f), 30f);
+        float alt = partyStat.altitude - startAltitude;
+        Vector3 pos = new Vector3(0f, startY - (alt * 0.25f), 30f);
         t.position = pos;
 	}
 
     public void SetLevelBackground(int lvlIndex)
     {
         spriteRenderer.sprite = lvlBackgrounds[lvlIndex - 1];
+        GameObject query = GameObject.FindGameObjectWithTag("Party");
+        PartyStat partyStat = query.GetComponent<PartyStat>();
+        startAltitude = partyStat.altitude;
     }
 }
